798fb61ea829a9c64101b4cb614472714ee75cb6
Raphael Janka <135700986+RaphaelJanka@users.noreply.github.com>
2023-10-23 22:02:18 +0200

    about me section added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
 import { AtfComponent } from './atf/atf.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AtfComponent
+    AtfComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
